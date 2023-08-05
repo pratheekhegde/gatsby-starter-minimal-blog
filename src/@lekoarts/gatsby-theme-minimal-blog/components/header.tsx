120bda9650eaa4cb84ca00292a36a25527f81a00
pTk <pratheekhegde@users.noreply.github.com>
2023-08-05 11:19:41 +0530

    Update header.tsx

--- src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   const { navigation: nav } = useMinimalBlogConfig()
 
   return (
-    <header sx={{ mb: [3,2] }}>
+    <header sx={{ mb: [5, 6] }}>
       <Flex sx={{ alignItems: `center`, justifyContent: `space-between` }}>
          <HeaderTitle />
         <ColorModeToggle />
